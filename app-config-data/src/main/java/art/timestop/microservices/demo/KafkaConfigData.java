447fd175a15f727798a873313fa3c0d61cb7cf74
hrasity <hrasity@hotmail.com>
2025-07-22 21:13:38 +0300

    modified:   app-config-data/pom.xml 	new file:   app-config-data/src/main/java/art/timestop/microservices/demo/KafkaConfigData.java 	new file:   app-config-data/src/main/java/art/timestop/microservices/demo/config/RetryConfigData.java 	modified:   docker-compose/.env 	modified:   docker-compose/common.yml 	new file:   kafka/pom.xml 	new file:   kafka/src/main/java/art/timestop/microservices/demo/Main.java 	modified:   pom.xml 	deleted:    twitter-to-kafka-service/logs/twitter-to-kafka-service.log 	modified:   twitter-to-kafka-service/src/main/resources/application.yaml 	new file:   twitter-to-kafka-service/twitter-to-kafka-service/logs/archived/twitter-to-kafka-service-log.2025-07-21.0.log 	modified:   twitter-to-kafka-service/twitter-to-kafka-service/logs/twitter-to-kafka-service.log

---
+++ app-config-data/src/main/java/art/timestop/microservices/demo/KafkaConfigData.java
@@ -0,0 +1,21 @@
+package art.timestop.microservices.demo;
+
+import java.util.List;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix="kafka-config")
+public class KafkaConfigData {
+    private String bootstrapServers;
+    private String schemaRegistryUrlKey;
+    private String schemaRegistryUrl;
+    private String topicName;
+    private List<String> topicNamesToCreate;
+    private Integer numOfPartitions;
+    private Short replicationFactor;
+}
